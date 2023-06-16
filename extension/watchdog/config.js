c833d6051ead5b127112cc3a8f06be2e296693ad
bob <aalbayda@up.edu.ph>
2023-06-16 13:41:41 +0800

    added config.js

---
+++ extension/watchdog/config.js
@@ -0,0 +1,12 @@
+const config = {
+	apiKey: "INSERT_YOUR_API_KEY_HERE", // can't use mine as it's now rate-limited
+	triggers: [
+		"War and Gore",
+		"Body Dysmorphia",
+		"Sexual Violence",
+		"Gender-related Trauma",
+		"Race-related Trauma",
+	],
+};
+
+module.exports = config;
