237039990765ae2b45d111dbf907eda65f84e10c
anonSupL <yilun.luxue@gmail.com>
2023-06-17 23:27:26 -0700

    save db as local file

--- backend/repository/UserRepo.js
+++ backend/repository/UserRepo.js
@@ -1,5 +1,8 @@
 const { Sequelize, Model, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('sqlite::memory:');
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './db.sqlite'
+});
 
 
 
