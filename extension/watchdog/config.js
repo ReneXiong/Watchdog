91e31b0bf80baa4d5fd9c73c2f57d5833edb2ac9
bob <aalbayda@up.edu.ph>
2023-06-18 17:12:16 +0800

    protected apis

--- extension/watchdog/config.js
+++ extension/watchdog/config.js
@@ -1,13 +1,5 @@
 const config = {
 	apiKey: "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",
-	triggers: [
-		"War and gore",
-		"Race-based hate",
-		"Gender-based hate",
-		"Body image hate",
-		"Sexual violence",
-		"Suicide and self harm",
-	],
 };
 
 module.exports = config;
